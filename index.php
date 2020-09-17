e763581b1508ce872487f1b85ed07ee74ab6c56d
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2020-09-17 19:46:10 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+session_start(); 
+
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <title>Login</title>
+  <meta charset="utf-8">
+  <link href="../CSS/style.css" rel="stylesheet" type="text/css" />
+  <link href="../CSS/bootstrap.min.css" rel="stylesheet" type="text/css" />
+  <link href="../CSS/bootstrap-theme.min.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+
+  <div class="row">
+    <div class=" ">
+      <div class="container">
+
+        
+
+      </div>
+
+    </div>
+
+
+
+
+  </div>
+
+
+</body>
+<script type="text/javascript" src="../Scripts/bootstrap.min.js"></script>
+<script type="text/javascript" src="../Scripts/jquery-3.1.1.min.js"></script>
+<script type="text/javascript" src="../Scripts/validacao.js"></script>
+
+</html>
+
