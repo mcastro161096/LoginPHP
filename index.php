9c1ac26ddce70659f85a8411cb8646c9bb2ed92d
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2020-09-17 21:08:21 -0300

    Tela de login concluída.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-session_start(); 
+session_start();
 
 
 ?>
@@ -9,29 +9,65 @@ session_start();
 <head>
   <title>Login</title>
   <meta charset="utf-8">
-  <link href="../CSS/style.css" rel="stylesheet" type="text/css" />
-  <link href="../CSS/bootstrap.min.css" rel="stylesheet" type="text/css" />
-  <link href="../CSS/bootstrap-theme.min.css" rel="stylesheet" type="text/css" />
+  <link href="../LoginPHP/CSS/style.css" rel="stylesheet" type="text/css" />
+  <link href="../LoginPHP/CSS/bootstrap.min.css" rel="stylesheet" type="text/css" />
+  <link href="../LoginPHP/CSS/bootstrap-theme.min.css" rel="stylesheet" type="text/css" />
 
 </head>
 
 <body>
+  <div class="container" style="margin-top: 40px">
+    <div class="row">
 
-  <div class="row">
-    <div class=" ">
-      <div class="container">
+      <div class="col-sm-6 col-md-4 col-md-offset-4">
+        <div class="panel panel-default">
 
-        
+          <div class="panel-body">
 
+            <form>
+
+              <fieldset>
+               
+                <div class="row">
+                  <div class="col-sm-12 col-md-10  col-md-offset-1 ">
+                    <div class="form-group">
+                      <div class="input-group">
+                        <span class="input-group-addon">
+                          <i class="glyphicon glyphicon-user"> </i>
+                        </span>
+                        <input type="email" class="form-control input-lg" placeholder = "Informe seu Email"/>
+                        
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <div class="input-group">
+                        <span class="input-group-addon">
+                          <i class="glyphicon glyphicon-lock"> </i>
+                        </span>
+                        <input type="password" class="form-control input-lg" placeholder = "Informe a senha"/>
+                        
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <input type="submit" class="btn btn-lg btn-primary btn-block" value="Continuar">
+                      <input type="reset" class="btn-link pull-left" value="Limpar"/>
+                      <a href="https://localhost/LoginPHP/View/Cadastro.php" class="pull-right">Criar conta</a>
+
+                    </div>
+                  </div>
+                </div>
+              </fieldset>
+
+            </form>
+
+          </div>
+          <div class="panel-footer ">
+          <a href="https://www.ulbra.br">Instituição</a>
+          </div>
+        </div>
       </div>
-
     </div>
-
-
-
-
   </div>
-
 
 </body>
 <script type="text/javascript" src="../Scripts/bootstrap.min.js"></script>
@@ -39,4 +75,3 @@ session_start();
 <script type="text/javascript" src="../Scripts/validacao.js"></script>
 
 </html>
-
