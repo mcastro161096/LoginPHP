d0da5ca867ae9e4773a198c1bb0d4325666d3957
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2020-09-21 07:10:24 -0300

    Criado o método Cadastrar.

--- Repositorio/UsuariosRepositorio.php
+++ Repositorio/UsuariosRepositorio.php
@@ -1,6 +1,17 @@
 <?php
-$conexao = new mysqli("localhost", "root", "", "dbphp7");
 
+    function Cadastrar(Usuario $usuario)
+    {
+        $conexao = new mysqli("localhost", "root", "", "dbphp7");
+            $query = $conexao->prepare("INSERT INTO tb_usuarios 
+                (Login, Senha, Nome, Sobrenome, DataNascimento, Escolaridade, Profissao) 
+                    VALUES (?, ?, ?, ?, ?, ? ?:);");
+                $query->bind_param("sssssss",$usuario->getLogin(),
+                    $usuario->getSenha(), $usuario->getNome(), $usuario->getSobrenome(), $usuario->getDataNascimento(),
+                         $usuario->getEscolaridade(), $usuario->getProfissao());
+
+          $query->execute();               
+    }
 
 
 
